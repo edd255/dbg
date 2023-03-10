50701c3e19e2e7d9a9e63e8b581e1ff118d9ee3b
edd255 <abaddon@disroot.org>
2023-03-10 15:49:09 +0100

    style: prefixed member variables with m_

--- include/breakpoint.hpp
+++ include/breakpoint.hpp
@@ -7,37 +7,47 @@
 namespace dbg {
     class Breakpoint {
         private:
-            pid_t process_id;
-            std::intptr_t address;
-            bool enabled;
-            uint8_t saved_data;
+            pid_t m_process_id;
+            std::intptr_t m_address;
+            bool m_enabled;
+            uint8_t m_saved_data;
 
         public:
             Breakpoint() = default;
-            Breakpoint(pid_t process_id, std::intptr_t address) :
-                process_id{process_id}, address{address}, enabled{false}, saved_data{}
-            {}
+            Breakpoint(pid_t process_id, std::intptr_t address)
+            {
+                m_process_id = process_id;
+                m_address = address;
+                m_enabled = false;
+            }
 
             void enable()
             {
-                auto data = ptrace(PTRACE_PEEKDATA, process_id, address, nullptr);
-                saved_data = static_cast<uint8_t>(data & 0xff);
+                auto data = ptrace(PTRACE_PEEKDATA, m_process_id, m_address, nullptr);
+                m_saved_data = static_cast<uint8_t>(data & 0xff);
                 uint64_t int3 = 0xcc;
                 uint64_t data_with_int3 = ((data & ~0xff) | int3);
-                ptrace(PTRACE_POKEDATA, process_id, address, data_with_int3);
-                enabled = true;
+                ptrace(PTRACE_POKEDATA, m_process_id, m_address, data_with_int3);
+                m_enabled = true;
             }
 
             void disable()
             {
-                auto data = ptrace(PTRACE_PEEKDATA, process_id, address, nullptr);
-                auto restored_data = ((data & ~0xff) | saved_data);
-                ptrace(PTRACE_POKEDATA, process_id, address, restored_data);
-                enabled = false;
+                auto data = ptrace(PTRACE_PEEKDATA, m_process_id, m_address, nullptr);
+                auto restored_data = ((data & ~0xff) | m_saved_data);
+                ptrace(PTRACE_POKEDATA, m_process_id, m_address, restored_data);
+                m_enabled = false;
             }
 
-            auto is_enabled() const -> bool { return enabled; }
-            auto get_address() const -> std::intptr_t { return address; }
+            bool is_enabled()
+            {
+                return m_enabled;
+            }
+
+            std::intptr_t get_address()
+            {
+                return m_address;
+            }
     };
 }
 
