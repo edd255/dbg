e9588cc636e730212ae565be3f63e5dddd40831a
edd255 <abaddon@disroot.org>
2023-03-10 11:35:14 +0100

    feat: completed part five

--- src/debugger.hpp
+++ src/debugger.hpp
@@ -1,42 +1,58 @@
 #ifndef DEBUGGER_H
 #define DEBUGGER_H
 
-#include <iostream>
-#include <iomanip>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/ptrace.h>
-#include <vector>
-#include <sstream>
+#include <fcntl.h>
+#include <utility>
+#include <string>
+#include <signal.h>
 #include <unordered_map>
+
 #include "breakpoint.hpp"
 #include "registers.hpp"
 #include "../libs/linenoise/linenoise.h"
+#include "../libs/libelfin/elf/elf++.hh"
+#include "../libs/libelfin/dwarf/dwarf++.hh"
 
 class Debugger
 {
     private:
-        std::string program_name;
-        pid_t process_id;
-        std::unordered_map<std::intptr_t, Breakpoint> breakpoints;
+        std::string m_program_name;
+        pid_t m_process_id;
+        std::unordered_map<std::intptr_t, Breakpoint> m_breakpoints;
+        dwarf::dwarf m_debug_info;
+        elf::elf m_elf;
+        uint64_t m_load_address = 0;
 
-    public:
-        Debugger(std::string program_name, pid_t process_id) :
-            program_name{std::move(program_name)}, process_id{process_id}
-        {}
-        void run();
         void handle_command(const std::string& line);
-        std::vector<std::string> split(const std::string& s, char delimiter);
-        bool is_prefix(const std::string& s, const std::string& of);
+        static std::vector<std::string> split(const std::string& s, char delimiter);
+        static bool is_prefix(const std::string& s, const std::string& of);
         void continue_execution();
-        void set_breakpoint_at_address(std::intptr_t address);
-        void dump_registers();
-        uint64_t read_memory(uint64_t address);
-        void write_memory(uint64_t address, uint64_t value);
-        uint64_t get_program_counter();
-        void set_program_conter(uint64_t program_counter);
+        uint64_t read_memory(uint64_t address) const;
+        void write_memory(uint64_t address, uint64_t value) const;
+        uint64_t get_program_counter() const;
+        void set_program_counter(uint64_t program_counter) const;
         void step_over_breakpoint();
         void wait_for_signal();
+        dwarf::die get_function_from_pc(uint64_t pc);
+        dwarf::line_table::iterator get_line_entry_from_pc(uint64_t pc);
+        void initialise_load_address();
+        uint64_t offset_load_address(uint64_t addr) const;
+        siginfo_t get_signal_info() const;
+        void handle_sigtrap(siginfo_t info);
+
+    public:
+        Debugger(std::string program_name, pid_t process_id)
+        {
+            m_program_name = std::move(program_name);
+            m_process_id = process_id;
+            auto fd = open(m_program_name.c_str(), O_RDONLY);
+            m_elf = elf::elf{elf::create_mmap_loader(fd)};
+            m_debug_info = dwarf::dwarf{dwarf::elf::create_loader(m_elf)};
+        }
+        void run();
+        void set_breakpoint_at_address(std::intptr_t address);
+        void dump_registers() const;
+        static void print_source(const std::string& file_name, unsigned line, unsigned n_lines_context = 2);
 };
 
 #endif
